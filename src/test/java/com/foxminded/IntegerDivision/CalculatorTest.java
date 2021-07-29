a4a8593960433324ba7b876a6a13be308d886f0a
Maxim Sheyko <maxim37377373@gmail.com>
2021-07-29 14:51:18 +0400

    deleted called methods in constant variables

--- src/test/java/com/foxminded/IntegerDivision/CalculatorTest.java
+++ src/test/java/com/foxminded/IntegerDivision/CalculatorTest.java
@@ -3,53 +3,58 @@ package com.foxminded.IntegerDivision;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    
     Calculator calculator;
-    private final static long DIVISOR_IS_ZERO = 0;
-    private final static long DEFAULT_DIVIDER = 46778;
-    private final static long DEFAULT_DIVIDER_NEGATIVE = -46778;
-    private final static long DEFAULT_DIVISOR = 47;
-    private final static String ALL_MINUEND_AND_SUBTRAHEND = "[423, 447, 423, 248, 235, 13]";
-    private final static String ALL_OFFSETS = "[0, 1, 1, 2, 2, 3]";
-    private final static long QUANTITY = 995;
-    private final static long QUANTITY_NEGATIVE = -995;
+    final static long DIVISOR_IS_ZERO = 0;
+    final static long DEFAULT_DIVIDER = 46778;
+    final static long DEFAULT_DIVIDER_NEGATIVE = -46778;
+    final static long DEFAULT_DIVISOR = 47;
+    final static String ALL_MINUEND_AND_SUBTRAHEND = "[423, 447, 423, 248, 235, 13]";
+    final static String ALL_OFFSETS = "[0, 1, 1, 2, 2, 3]";
+    final static long QUANTITY = 995;
+    final static long QUANTITY_NEGATIVE = -995;
+
 
     @BeforeEach
-    void init() {
-	calculator = new Calculator();
+    void init(){
+        calculator = new Calculator();
     }
 
     @Test
     void getMathData_whenDivisorIsZero() {
-	assertThrows(IllegalArgumentException.class, () -> calculator.getMathData(DEFAULT_DIVIDER, DIVISOR_IS_ZERO));
+        assertThrows(IllegalArgumentException.class, () -> calculator.getMathData(DEFAULT_DIVIDER,DIVISOR_IS_ZERO));
     }
 
     @Test
     void getMathData_NO_NULL() {
-	assertNotNull(calculator.getMathData(DEFAULT_DIVIDER, DEFAULT_DIVISOR));
+        assertNotNull(calculator.getMathData(DEFAULT_DIVIDER,DEFAULT_DIVISOR));
     }
 
     @Test
     void getMathData_allMinuendAndSubtrahendCheck() {
-	assertEquals(ALL_MINUEND_AND_SUBTRAHEND,
-		calculator.getMathData(DEFAULT_DIVIDER, DEFAULT_DIVISOR).getMinuendAndSubtrahend().toString());
+        assertEquals(ALL_MINUEND_AND_SUBTRAHEND,
+                calculator.getMathData(DEFAULT_DIVIDER,DEFAULT_DIVISOR).getMinuendAndSubtrahend().toString());
     }
 
     @Test
     void getMathData_allOffsetsCheck() {
-	assertEquals(ALL_OFFSETS, calculator.getMathData(DEFAULT_DIVIDER, DEFAULT_DIVISOR).getOffsets().toString());
+        assertEquals(ALL_OFFSETS,
+                calculator.getMathData(DEFAULT_DIVIDER,DEFAULT_DIVISOR).getOffsets().toString());
     }
 
     @Test
     void getMathData_QuantityPositiveCheck() {
-	assertEquals(QUANTITY, calculator.getMathData(DEFAULT_DIVIDER, DEFAULT_DIVISOR).getQuantity());
+        assertEquals(QUANTITY, calculator.getMathData(DEFAULT_DIVIDER,DEFAULT_DIVISOR).getQuantity());
     }
 
     @Test
     void getMathData_QuantityNegativeCheck() {
-	assertEquals(QUANTITY_NEGATIVE,
-		calculator.getMathData(DEFAULT_DIVIDER_NEGATIVE, DEFAULT_DIVISOR).getQuantity());
+        assertEquals(QUANTITY_NEGATIVE,
+                calculator.getMathData(DEFAULT_DIVIDER_NEGATIVE,DEFAULT_DIVISOR).getQuantity());
     }
 }
--- src/test/java/com/foxminded/IntegerDivision/FormatterTest.java
+++ src/test/java/com/foxminded/IntegerDivision/FormatterTest.java
@@ -12,39 +12,36 @@ class FormatterTest {
     private static final long DEFAULT_DIVISOR = 4;
     private static final long DIVIDER_IS_GREATER = 144;
     private static final long DIVIDER_IS_GREATER_IS_NEGATIVE = -144;
-    private final String TABLE_DIVIDER_IS_ZERO_EXPECTED = String.format("_0|%d%2$s" + " 0|-%2$s" + " ˉ|0%2$s",
+    private static final String TABLE_DIVIDER_IS_ZERO_EXPECTED = String.format("_0|%d%2$s" + " 0|-%2$s" + " ˉ|0%2$s",
 	    DEFAULT_DIVISOR, SEPARATOR_LINE);
-    private final String TABLE_DIVIDER_IS_ZERO_ACTUAL = formatter
-	    .getForm(calculator.getMathData(DIVIDER_IS_ZERO, DEFAULT_DIVISOR));
-    private final String TABLE_DIVIDER_IS_GREATER_EXPECTED = String.format(
+    private static final String TABLE_DIVIDER_IS_GREATER_EXPECTED = String.format(
 	    "_144|%d%2$s" + " 12 |--%2$s" + " ˉˉ |36%2$s" + " _24%2$s" + "  24%2$s" + "  ˉˉ%2$s" + "   0",
 	    DEFAULT_DIVISOR, SEPARATOR_LINE);
-    private final String TABLE_DIVIDER_IS_GREATER_ACTUAL = formatter
-	    .getForm(calculator.getMathData(DIVIDER_IS_GREATER, DEFAULT_DIVISOR));
-    private final String TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_EXPECTED = String.format(
+    private static final String TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_EXPECTED = String.format(
 	    "_144|%d%2$s" + " 12 |---%2$s" + " ˉˉ |-36%2$s" + " _24%2$s" + "  24%2$s" + "  ˉˉ%2$s" + "   0",
 	    DEFAULT_DIVISOR, SEPARATOR_LINE);
-    private final String TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_ACTUAL = formatter
-	    .getForm(calculator.getMathData(DIVIDER_IS_GREATER_IS_NEGATIVE, DEFAULT_DIVISOR));
 
     @Test
     void getForm_NO_NULL() {
-	assertNotNull(TABLE_DIVIDER_IS_ZERO_ACTUAL);
+	assertNotNull(formatter
+		    .getForm(calculator.getMathData(DIVIDER_IS_ZERO, DEFAULT_DIVISOR)));
     }
 
     @Test
     void getForm_whenDividerIsGreaterDivisor() {
-	assertEquals(TABLE_DIVIDER_IS_GREATER_EXPECTED, TABLE_DIVIDER_IS_GREATER_ACTUAL);
+	assertEquals(TABLE_DIVIDER_IS_GREATER_EXPECTED, formatter
+		    .getForm(calculator.getMathData(DIVIDER_IS_GREATER, DEFAULT_DIVISOR)));
     }
 
     @Test
     void getForm_whenDividerIsZero() {
-	assertEquals(TABLE_DIVIDER_IS_ZERO_EXPECTED, TABLE_DIVIDER_IS_ZERO_ACTUAL);
+	assertEquals(TABLE_DIVIDER_IS_ZERO_EXPECTED,formatter
+		    .getForm(calculator.getMathData(DIVIDER_IS_ZERO, DEFAULT_DIVISOR)));
     }
 
     @Test
     void getForm_whenDividerOrDivisorHaveNegativeSign() {
-	assertEquals(TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_EXPECTED,
-		TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_ACTUAL);
+	assertEquals(TABLE_DIVIDER_OR_DIVISOR_HAVE_NEGATIVE_SIGN_EXPECTED, formatter
+		    .getForm(calculator.getMathData(DIVIDER_IS_GREATER_IS_NEGATIVE, DEFAULT_DIVISOR)));
     }
 }
